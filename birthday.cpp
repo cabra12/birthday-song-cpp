ee684fe1a26023e34354e71e4e29c5140c53187f
cabra12 <charlottemabraham32@gmail.com>
2025-05-27 12:53:19 -0400

    birthday.cpp

--- birthday.cpp
+++ birthday.cpp
@@ -8,16 +8,15 @@ int main(){
     std::cout << "What is your name?: "; 
     std::cin >> name; 
 
-    happyBirthday();  //parenthesis invoke function, think of as telephone
+    happyBirthday();  
     
     return 0;
 }
 
 void happyBirthday(){
+    std::cout << '\n';
     for(int i = 0; i < 2; i++){
-        std::cout << "Happy Birthday " << '\n'; 
+        std::cout << "Happy Birthday to you!" << '\n'; 
     }
     std::cout << "Happy Birthday dear " << name << "!" << '\n';
     std::cout << "Happy Birthday to you!"<< '\n';
-    
-}
