268362b0146fcc28ce3caeae6bd00fb8227d5318
cabra12 <charlottemabraham32@gmail.com>
2025-05-27 13:19:52 -0400

    birthday.cpp

--- birthday.cpp
+++ birthday.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 
-std::string name;
-void happyBirthday();
+void happyBirthday(std::string name, int age);
 
 int main(){
+    std::string name;
+    int age;
 
     std::cout << "What is your name?: "; 
     std::cin >> name; 
 
-    happyBirthday();  
+    std::cout << "How old are you today?: ";
+    std::cin >> age;
+
+    happyBirthday(name, age);  //parenthesis invoke function, think of as telephone, variable has to be run through
     
     return 0;
 }
 
-void happyBirthday(){
+void happyBirthday(std::string name, int age){
     std::cout << '\n';
+    std::cout << "Here's your song: " << '\n';
     for(int i = 0; i < 2; i++){
         std::cout << "Happy Birthday to you!" << '\n'; 
     }
     std::cout << "Happy Birthday dear " << name << "!" << '\n';
     std::cout << "Happy Birthday to you!"<< '\n';
+    std::cout << "You are " << age << " years old!" << '\n';
+    
+}
